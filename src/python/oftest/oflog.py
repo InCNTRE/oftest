2b200167fd23ff789581c5cae578e37a6599270b
Jonathan Stout <jonstout@indiana.edu>
2015-02-03 12:15:08 -1000

    fixing logg lvl to DEBUG

--- src/python/oftest/oflog.py
+++ src/python/oftest/oflog.py
@@ -5,7 +5,12 @@ import logging
 import os
 import time
 from oftest import config
- 
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    DEVNULL = open(os.devnull, "wb")
+
 """
 oflog.py
 Provides Loggers for oftest cases, and easy to use wireshark
@@ -14,59 +19,14 @@ logging.
 Test case writers use three main functions.
 1. get_logger() - Returns a Logger for each testcase or the
 default logger if --publish is not passed.
-2. @wireshark_capture - Decorator Uses tshark to capture network
-traffic while function is being run.
- 
-oflog is configured using one method.
-1. set_config() - Records all logs under directory. directory
-*must* end in '/'. Also configures wireshark to log the interface
-associated with ctrlAddr and all other data plane interfaces.
+2. start_logging(testcase) - Starts tshark on each test interface,
+and the controller interface if -H is passed on the CLI. Additionally
+registers tshark pids to testcase for process clean up.
 """
  
 pubName = ""
-wiresharkMap = {}
-DEVNULL = None
 
-def wireshark_capture(f):
-    """
-    Decorator to wrap Testcases. Gives
-    a one second buffer for wireshark to
-    start and stop if publishing is enabled.
-    """
-    def pub(*args, **kargs):
-        create_log_directory(str(args[0].__class__.__name__))
-        start_wireshark()
-        time.sleep(3)
-        try:
-            f(*args, **kargs)
-        finally:
-            stop_wireshark()
-            time.sleep(3)
-
-    if not config["publish"]:
-        return f
-    else:
-        return pub
-
-def create_log_directory(dirName):
-    """
-    Creates a directory named dirName. Also save dirName as a
-    global variable to inform get_logger() where to log to.
-    """
-    global pubName
-    pubName = dirName
-    logDir = "%slogs/%s" % ("./src/python/ofreport/", pubName)
-    print logDir
-    try:
-        Popen(["rm", "-rf", logDir],stdout=None)
-        time.sleep(1)
-    except:
-        pass
-    finally:
-        os.makedirs(logDir)
-
-
-def create_log_directory2(log_directory):
+def create_log_directory(log_directory):
     """Deletes directory, (if already exists) and then recreates
     directory.
     """
@@ -81,7 +41,7 @@ def create_log_directory2(log_directory):
 def start_wireshark_cap(log_directory):
     process_ids = []
     interfaces = config["port_map"].values()
-    interfaces.append(find_iface(config["controller_host"]))
+    interfaces.append(find_interface(config["controller_host"]))
 
     for iface in interfaces:
         fd = log_directory + "{0}.pcap".format(iface)
@@ -101,9 +61,11 @@ def start_logging(testcase):
     if not config["publish"]: return
 
     _group, _test = testcase.__class__.__name__[3:].split("No")
+    global pubName
+    pubName = "Grp{0}No{1}".format(_group, _test)
     directory = "./src/python/ofreport/logs/Grp{0}No{1}/"
     log_directory = directory.format(_group, _test)
-    create_log_directory2(log_directory)
+    create_log_directory(log_directory)
 
     process_ids = start_wireshark_cap(log_directory)
     testcase.addCleanup(stop_logging, process_ids)
@@ -119,7 +81,7 @@ def get_logger():
     if not config["publish"]:
         return logging
     LOG = logging.getLogger(pubName)
-    LOG.setLevel(config["dbg_level"])
+    LOG.setLevel(logging.DEBUG)
     logDir = "%slogs/%s" % ("./src/python/ofreport/", pubName)
     h = logging.FileHandler(logDir+"/testcase.log")
     h.setLevel(logging.DEBUG)
@@ -128,32 +90,8 @@ def get_logger():
     h.setFormatter(f)
     LOG.addHandler(h)
     return LOG
- 
-def start_wireshark():
-    for iface in wiresharkMap:
-        fd = "%slogs/%s/%s.pcap" % ("./src/python/ofreport/", pubName, wiresharkMap[iface][1])
-        wiresharkMap[iface][0] = Popen(["tshark", "-i", str(iface), "-w", fd, "-q"], stdout=DEVNULL, stderr=DEVNULL)
 
-def stop_wireshark():
-    for iface in wiresharkMap:
-        wiresharkMap[iface][0].terminate()
-
-def set_config():
-    if not config["publish"]:
-        return
-    global wiresharkMap
-    global DEVNULL
-    DEVNULL = open(os.devnull, 'w')
-
-    for k in config["port_map"]:
-        iface = config["port_map"][k]
-        # [pid, "dataX"]
-        wiresharkMap[iface] = [None, "data"+str(k)]
-    # Controller's iface is not included in a config. Look it up.
-    iface = find_iface(config["controller_host"])
-    wiresharkMap[iface] = [None, "ctrl"]
-
-def find_iface(ip="127.0.0.1"):
+def find_interface(ip="127.0.0.1"):
     """
     Parses ifconfig to return the interface associated with ip.
     """
